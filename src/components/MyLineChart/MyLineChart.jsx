c6af165244c19121d26869072a2548ee1dd7c07e
progmamun <mamun333089@gmail.com>
2022-03-29 18:17:17 +0600

    simple-line-chart-done

--- src/components/MyLineChart/MyLineChart.jsx
+++ src/components/MyLineChart/MyLineChart.jsx
@@ -1,54 +1,56 @@
 import React from 'react';
-import { Line, LineChart, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const MyLineChart = () => {
   const data = [
     {
       name: 'Supplier A',
       price: 4000,
-      pv: 2400,
+      sales: 2400,
       amt: 2400,
     },
     {
       name: 'Supplier B',
       price: 3000,
-      pv: 1398,
+      sales: 1398,
       amt: 2210,
     },
     {
       name: 'Supplier C',
       price: 2000,
-      pv: 9800,
+      sales: 9800,
       amt: 2290,
     },
     {
       name: 'Supplier D',
       price: 2780,
-      pv: 3908,
+      sales: 3908,
       amt: 2000,
     },
     {
       name: 'Supplier E',
       price: 1890,
-      pv: 4800,
+      sales: 4800,
       amt: 2181,
     },
     {
       name: 'Supplier F',
       price: 2390,
-      pv: 3800,
+      sales: 3800,
       amt: 2500,
     },
     {
       name: 'Supplier G',
       price: 3490,
-      pv: 4300,
+      sales: 4300,
       amt: 2100,
     },
   ];
   return (
     <LineChart width={800} height={500} data={data}>
       <Line dataKey={'price'}></Line>
+      <Line dataKey={'sales'}></Line>
+      <Tooltip></Tooltip>
       <XAxis dataKey="name"></XAxis>
       <YAxis></YAxis>
     </LineChart>
